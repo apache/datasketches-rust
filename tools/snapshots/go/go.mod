141b4747c4de130bca92e5cca42cc5d1d0158cfc
tison <wander4096@gmail.com>
2025-12-15 23:03:08 +0800

    chore: check generated snapshots
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ tools/snapshots/go/go.mod
@@ -0,0 +1,15 @@
+module datasketches.apache.org/tools
+
+go 1.25.5
+
+require (
+	github.com/apache/datasketches-go v0.0.0-20251212084617-f7bc4b1db865
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/twmb/murmur3 v1.1.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ tools/snapshots/go/tools_test.go
@@ -0,0 +1,54 @@
+package tools
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/apache/datasketches-go/hll"
+	"github.com/stretchr/testify/require"
+)
+
+const defaultLgK = 12
+
+func TestGenerateGoFile(t *testing.T) {
+	path, err := os.Getwd()
+	require.NoError(t, err)
+
+	workspace := filepath.Join(path, "..", "..", "..")
+	println("%v", workspace)
+
+	nArr := []int{0, 1, 10, 100, 1000, 10000, 100000, 1000000}
+	for _, n := range nArr {
+		hll4, err := hll.NewHllSketch(defaultLgK, hll.TgtHllTypeHll4)
+		require.NoError(t, err)
+		hll6, err := hll.NewHllSketch(defaultLgK, hll.TgtHllTypeHll6)
+		require.NoError(t, err)
+		hll8, err := hll.NewHllSketch(defaultLgK, hll.TgtHllTypeHll8)
+		require.NoError(t, err)
+
+		for i := 0; i < n; i++ {
+			require.NoError(t, hll4.UpdateUInt64(uint64(i)))
+			require.NoError(t, hll6.UpdateUInt64(uint64(i)))
+			require.NoError(t, hll8.UpdateUInt64(uint64(i)))
+		}
+		err = os.MkdirAll(workspace, os.ModePerm)
+		require.NoError(t, err)
+
+		sl4, err := hll4.ToCompactSlice()
+		require.NoError(t, err)
+		err = os.WriteFile(fmt.Sprintf("%s/hll4_n%d_go.sk", workspace, n), sl4, 0644)
+		require.NoError(t, err)
+
+		sl6, err := hll6.ToCompactSlice()
+		require.NoError(t, err)
+		err = os.WriteFile(fmt.Sprintf("%s/hll6_n%d_go.sk", workspace, n), sl6, 0644)
+		require.NoError(t, err)
+
+		sl8, err := hll8.ToCompactSlice()
+		require.NoError(t, err)
+		err = os.WriteFile(fmt.Sprintf("%s/hll8_n%d_go.sk", workspace, n), sl8, 0644)
+		require.NoError(t, err)
+	}
+}
