182879597c16e7903d2caa33820a75d3a0712de7
tison <wander4096@gmail.com>
2026-08-25 09:00:34 +0800

    Apply suggestion from @tisonkun

--- datasketches/src/req/union.rs
+++ datasketches/src/req/union.rs
@@ -24,7 +24,7 @@ use crate::error::Error;
 
 /// Combines multiple [`ReqSketch`]es into a single result.
 ///
-/// Internally wraps a `ReqSketch` configured for union semantics. 
+/// Internally wraps a `ReqSketch` configured for union semantics.
 #[derive(Debug, Clone)]
 pub struct ReqUnion<T: ReqValue> {
     inner: ReqSketch<T>,
